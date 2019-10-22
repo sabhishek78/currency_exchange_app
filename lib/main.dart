8669804e0ef3ca13b0b546067be623b6462d44a2
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-22 22:52:13 +0530

    exchange rate app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: CurrencyExchange(),
-  ));
-}
+void main() => runApp(new MaterialApp(
+  title: "Currency Converter",
+  home: CurrencyExchange(),
+));
 
 class CurrencyExchange extends StatefulWidget {
   @override
@@ -12,8 +15,126 @@ class CurrencyExchange extends StatefulWidget {
 }
 
 class _CurrencyExchangeState extends State<CurrencyExchange> {
+  final fromTextController = TextEditingController();
+  List<String> currencies;
+  String fromCurrency = "USD";
+  String toCurrency = "INR";
+  String result;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCurrencies();
+  }
+
+  Future<String> _loadCurrencies() async {
+    String uri = "http://api.openrates.io/latest";
+    var response = await http.get(Uri.encodeFull(uri), headers: {"Accept": "application/json"});
+    var responseBody = json.decode(response.body);
+    Map curMap = responseBody['rates'];
+    currencies = curMap.keys.toList();
+    setState(() {});
+    print(currencies);
+    return "Success";
+  }
+
+  Future<String> _doConversion() async {
+    String uri = "http://api.openrates.io/latest?base=$fromCurrency&symbols=$toCurrency";
+    var response = await http
+        .get(Uri.encodeFull(uri), headers: {"Accept": "application/json"});
+    var responseBody = json.decode(response.body);
+    setState(() {
+      result = (double.parse(fromTextController.text) * (responseBody["rates"][toCurrency])).toStringAsFixed(2);
+    });
+    print(result);
+    return "Success";
+  }
+
+  _onFromChanged(String value) {
+    setState(() {
+      fromCurrency = value;
+    });
+  }
+
+  _onToChanged(String value) {
+    setState(() {
+      toCurrency = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Exchange Rates"),
+        backgroundColor: Colors.red,
+      ),
+      body: currencies == null
+          ? Center(child: CircularProgressIndicator())
+          : Container(
+        color: Colors.lightBlueAccent,
+       // height: MediaQuery.of(context).size.height / 2,
+      //  width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            elevation: 3.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ListTile(
+                  title: TextField(
+                    controller: fromTextController,
+                    style: TextStyle(fontSize: 20.0, color: Colors.red),
+                    keyboardType:
+                    TextInputType.numberWithOptions(decimal: true),
+                  ),
+                  trailing: _buildDropDownButton(fromCurrency),
+                ),
+                IconButton(
+                  icon: Icon(Icons.autorenew),
+                  color: Colors.red,
+                  iconSize: 100,
+                  onPressed: _doConversion,
+                ),
+                ListTile(
+                  title: Chip(
+                    label: result != null ? Text(result,
+                      style: Theme.of(context).textTheme.display2,
+
+                    ) : Text(""),
+                  ),
+                  trailing: _buildDropDownButton(toCurrency),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDropDownButton(String currencyCategory) {
+    return DropdownButton(
+      value: currencyCategory,
+      items: currencies
+          .map((String value) => DropdownMenuItem(
+        value: value,
+        child: Row(
+          children: <Widget>[
+            Text(value),
+          ],
+        ),
+      ))
+          .toList(),
+      onChanged: (String value) {
+        if(currencyCategory == fromCurrency){
+          _onFromChanged(value);
+        }else {
+          _onToChanged(value);
+        }
+      },
+    );
   }
 }
